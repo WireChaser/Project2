f3ccba6bc7940e4bff5c5e0c036fece724153f34
Jordi Marcial Cruz <jordi.marcialcruz@gmail.com>
2024-08-04 00:38:51 -0400

    FIFO.sv updated with counter

--- FIFO.sv
+++ FIFO.sv
@@ -1,13 +1,3 @@
-/*  
- *  Create a FIFO (First In First Out) buffer with depth 4 using the given
- *  interface and constraints
- *    - The buffer is initally empty
- *    - Reads are combinational, so data_out is valid unless empty is asserted
- *    - Removal from the queue is processed on the clock edge.
- *    - Writes are processed on the clock edge
- *    - If a write is pending while the buffer is full, do nothing
- *    - If a read is pending while the buffer is empty, do nothing
- */
 module FIFO 
 	(input logic clock, reset_n,
 	input logic wr, rd,
@@ -18,55 +8,38 @@ module FIFO
 	logic [31:0] data_queue [3:0]; 
 	logic [31:0] data_pkt;
 	logic [1:0] wr_ptr, rd_ptr;
-	logic pkt_empty, hazard, valid_rd, valid_wr;
+	logic [2:0] count;
+	logic hazard, valid_rd, valid_wr;
 	
 	// Status signals
-	assign full  = (wr_ptr == 3 && rd_ptr == 0) || (wr_ptr + 1'd1 == rd_ptr);
-	assign empty = (wr_ptr == rd_ptr);	// No more valid data to read in between pointers
-	
-	assign pkt_empty = wr && empty;
 	assign hazard = (wr && full) || (rd && empty);
+	assign valid_rd = rd && !empty;
+	assign valid_wr = wr && !full;
+	assign full = (count == 4);
+	assign empty = (count == 0);
+	
 	assign data_out = (!hazard) ? data_pkt : 'z;
 	
-	assign valid_rd = !pkt_empty && rd && !empty;
-	assign valid_wr = !pkt_empty && wr && !full;
-	
-	// Data Pkt Logic 
+	// Write and Read Logic 
 	always_ff @(posedge clock) begin
 		if (!reset_n) begin
 			data_pkt <= '0;
+			rd_ptr <= '0;
+			wr_ptr <= '0;
+			count <= '0;
 			data_queue[0] <= '0;
 			data_queue[1] <= '0;
 			data_queue[2] <= '0;
 			data_queue[3] <= '0;
-		end else if (pkt_empty) begin		// Stuck in infinte loop
-			data_pkt <= data_in;
 		end else if (valid_wr) begin
 			data_queue[wr_ptr] <= data_in;
+			wr_ptr <= wr_ptr + 2'd1;
+			count <= count + 3'd1;
 		end else if (valid_rd) begin 
 			data_pkt <= data_queue[rd_ptr];
-		end 
-	end
-	
-	// Write Pointer Logic 
-	always_ff @(posedge clock) begin
-		if (!reset_n) begin
-			wr_ptr <= 0;
-		end else if (valid_wr) begin // Only write if not full
-			data_queue[wr_ptr] <= data_in;
-			if (wr_ptr == 3) wr_ptr <= 0;
-         else wr_ptr <= wr_ptr + 2'd1; 
+			rd_ptr <= rd_ptr + 2'd1;
+			count <= count - 3'd1;
 		end
 	end
-	
-	// Read Pointer Logic 
-	always_ff @(posedge clock) begin
-		if (!reset_n) begin
-			rd_ptr <= 0;
-		end else if (valid_rd) begin // Only read if not empty
-			if (rd_ptr == 3) rd_ptr <= 0;
-         else rd_ptr <= rd_ptr + 2'd1; 
-		end
-	end	
 	
 endmodule : FIFO
