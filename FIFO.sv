be5d5cd46414e29aa062355532750f4aea8b296f
Jordi Marcial Cruz <jordi.marcialcruz@gmail.com>
2024-08-26 00:19:13 -0400

    Simplified FIFO Logic

--- FIFO.sv
+++ FIFO.sv
@@ -1,59 +1,45 @@
-module FIFO 
-	(input logic clock, reset_n,
-	input logic wr, rd,
-	input logic [31:0] data_in, 
-	output logic full, empty,
-	output logic [31:0] data_out);
+module FIFO #(parameter WIDTH = 32) (
+  input wire clock, reset_n,
+  input wire [WIDTH-1:0] data_in,
+  input wire we, //write enable
+  input wire re, //read enable
+  output logic full,
+  output logic empty,
+  output logic [WIDTH-1:0] data_out);
 
-	logic [31:0] data_queue [3:0]; 
-	logic [31:0] data_pkt;
-	logic [1:0] wr_ptr, rd_ptr;
-	logic [3:0] count;
-	logic hazard, valid_rd, valid_wr;
-	logic pkt_loaded;
-	
-	// Status signals
-	assign hazard = (wr && full) || (rd && empty);
-	assign valid_rd = rd && !empty;
-	assign valid_wr = wr && !full;
-	assign full = (count == 5);
-	assign empty = (count == 0);
-	
-	assign data_out = (!hazard) ? data_pkt : 'z;
-	
-	// Write and Read Logic 
-	always_ff @(posedge clock) begin
+  logic [3:0][WIDTH-1:0] queue;
+  logic [1:0] w_ptr, r_ptr;
+  logic [2:0] count;
+  logic valid_re, valid_we, hazard;
+  
+  assign valid_re = re && !empty;
+  assign valid_we = we && !full;
+  assign hazard = re && we && (count > 0);
+
+  assign full = (count == 3'd4 && !re);
+  assign empty = (count == 3'd0);
+  
+	always_ff @(posedge clock or negedge reset_n) begin
 		if (!reset_n) begin
-			data_pkt <= '0;
-			rd_ptr <= '0;
-			wr_ptr <= '0;
-			count <= '0;
-			data_queue <= '{default:'0};
-			pkt_loaded <= '0;
-		end else if (valid_wr) begin
-			count <= count + 4'd1;
-			if (!pkt_loaded && empty) begin
-				data_pkt <= data_in;
-				pkt_loaded <= 1'd1;
-			end else if (!pkt_loaded && !empty) begin
-				data_pkt <= data_queue[wr_ptr - rd_ptr];
-				data_queue[wr_ptr] <= data_in;
-				wr_ptr <= wr_ptr + 2'd1;
-				pkt_loaded <= 1'd1;
-				rd_ptr <= rd_ptr + 2'd1;
-			end else if (pkt_loaded && !empty) begin
-				data_queue[wr_ptr] <= data_in;
-				wr_ptr <= wr_ptr + 2'd1;
-			end
-		end else if (valid_rd) begin 
-			count <= count - 4'd1;
-			if (!pkt_loaded) begin 
-				data_pkt <= data_queue[rd_ptr];
-				rd_ptr <= rd_ptr + 2'd1;
-			end else if (pkt_loaded && !empty) begin 
-				pkt_loaded <= '0;
-			end
-		end
-	end
-	
-endmodule : FIFO
+			count <= 0; 
+			w_ptr <= 0; 
+			r_ptr <= 0; 
+			queue <= 0;
+		end else if (hazard) begin 
+         queue[w_ptr] <= data_in;
+         count <= count;
+         w_ptr <= w_ptr + 2'd1;
+         r_ptr <= r_ptr + 2'd1;
+		end else if (valid_we) begin
+        queue[w_ptr] <= data_in;
+        count <= count + 2'd1;
+        w_ptr <= w_ptr + 2'd1;
+      end else if (valid_re) begin
+        count <= count - 2'd1;
+        r_ptr <= r_ptr + 2'd1;
+      end
+  end  
+
+  assign data_out = queue[r_ptr];
+
+endmodule
